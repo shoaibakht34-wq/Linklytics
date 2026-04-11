bc22e3bf2f00c715e91de5cc21d3bbdcc5f4b5b9
Shoaib Akhtar <shoaibakht34@gmail.com>
2026-04-11 14:09:53 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: import.meta.env.VITE_BACKEND_URL || "https://url-shortener-sb-1-o661.onrender.com",
 });
