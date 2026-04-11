666b9a0e0fea8e25bc340a50da3fe0edb717955e
Shoaib Akhtar <shoaibakht34@gmail.com>
2026-04-11 15:06:31 +0530

    Add request interceptor for JWT token

--- src/api/api.js
+++ src/api/api.js
@@ -3,3 +3,14 @@ import axios from "axios";
 export default axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL || "https://url-shortener-sb-1-o661.onrender.com",
 });
+api.interceptors.request.use((config) => {
+    const token = JSON.parse(localStorage.getItem("JWT_TOKEN"));
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export default api;
